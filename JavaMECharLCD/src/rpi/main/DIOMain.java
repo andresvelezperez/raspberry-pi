ace37d4d1f5eb7790dfa9c828fc9e77fc55e8532
Andres Velez Perez <andresvelezperez@gmail.com>
2014-09-25 22:09:39 -0500

    Se agrega clase con main para ejecutar jdk + dio

---
+++ JavaMECharLCD/src/rpi/main/DIOMain.java
@@ -0,0 +1,35 @@
+
+package rpi.main;
+
+import com.adafruit.examples.lcd.ExampleCharLCD;
+import com.adafruit.examples.lcd.char_lcd;
+import com.adafruit.examples.lcd.char_lcd_mcp;
+import com.adafruit.examples.lcd.char_lcd_rgb;
+import com.adafruit.examples.lcd.char_lcd_rgb_pwm;
+import java.io.IOException;
+
+/**
+ *
+ * @author andresvelezperez
+ */
+public class DIOMain {
+
+    public static void main(String args[]) throws IOException, InterruptedException {
+        
+        ExampleCharLCD exampleCharLCD = null;
+        
+        //exampleCharLCD = new char_lcd();
+
+        //exampleCharLCD = new char_lcd_rgb();
+
+        //exampleCharLCD = new char_lcd_rgb_pwm();
+
+        exampleCharLCD = new char_lcd_mcp();
+        
+        
+        
+        exampleCharLCD.init();
+        exampleCharLCD.stop();
+    }
+
+}
